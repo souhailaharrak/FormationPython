582ecf28594b73735f6b09cc44f2e7eade656031
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2024-02-21 23:55:39 +0000

    les Names

---
+++ VariableNames.py
@@ -0,0 +1,17 @@
+"""A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)"""
+
+#Snake Case
+#Each word is separated by an underscore character:
+my_variable_name = "Python"
+
+#Pascal Case :Each word starts with a capital letter:
+MyVariableName = "Python1"
+
+
+#Camel Case : Each word, except the first, starts with a capital letter:
+
+myVariableName = "Python3"
+
