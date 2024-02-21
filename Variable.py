bfb16292254739b7ed8bf7acf0ad31a0b94988d6
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2024-02-21 23:43:16 +0000

    LesVariables

---
+++ Variable.py
@@ -0,0 +1,31 @@
+#Python has no command for declaring a variable.
+x=5
+y="Python"
+print(x)
+print(y)
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#*********************************************************
+# Casting
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+
+
+#**********************************************
+#Get the Type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+
+
+
