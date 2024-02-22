47d010e9329b20e98b2398509855c9f9fc4e8d95
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2024-02-22 00:12:28 +0000

    input and output

---
+++ Inputs_Outputs.py
@@ -0,0 +1,28 @@
+#The Python print() function is often used to output variables.
+#Example
+x = "Python is awesome"
+print(x)
+
+
+#In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error:
+x = 5
+y = "John"
+print(x + y)
+
+#The best way to output multiple variables in the print() function is to separate them with commas,
+#which even support different data types:
+x = 5
+y = "John"
+print(x, y)
+
+#An output is a way for the computer to communicate with the outside world.
+#A message displayed on the screen and the sound from a speaker are examples of outputs.
+
+
+ 
+
+"""🌟 inputs and outputs help machines communicate with the outside world
+
+🌟 the input() instruction allows the user to enter a value into your program
+
+🌟 the print() instruction is used to generate an output"""
