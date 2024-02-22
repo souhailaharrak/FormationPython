69be2f61a3e53e8e560e23bc133af6c95b1f18ef
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2024-02-22 00:36:16 +0000

    LES TYPES

---
+++ DataType.py
@@ -0,0 +1,28 @@
+"""Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType"""
+
+"""🌟 computers store and process different data types differently
+
+🌟 string is the data type for text
+
+🌟 integer and float are data types for numbers"""
+
+x = 5
+print(type(x))
+#*************************************
+# Take the name as input
+name = input()
+
+# Use concatenation to join 2 strings
+message = "Nice to meet you, " + name
+
+# Display the message to the user
+print(message)
+
+
