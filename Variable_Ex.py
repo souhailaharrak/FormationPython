0b01a76c67820c394bc3ced36622aa11c2ee33dd
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2024-02-25 13:43:17 +0000

    simple Example

---
+++ Variable_Ex.py
@@ -0,0 +1,19 @@
+#Declaration des variables
+a=20
+b=10
+
+# Addition
+Res=a+b
+print("Addition: " ,a , "et " ,b, "est ",Res)
+
+#Sustraction 
+Res=a-b
+print("Sustraction: " ,a , "et " ,b, "est ",Res)
+
+#multiplicastion  
+Res=a*b
+print("Multiplication: " ,a , "et " ,b, "est ",Res)
+
+#Devision
+Res=a//b
+print("Multiplication: " ,a , "et " ,b, "est ",Res)
