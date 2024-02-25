a2b5a0af43a12538fbe593e6a4846380f07405a0
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2024-02-25 13:42:52 +0000

    String

--- String.py
+++ String.py
@@ -6,3 +6,31 @@ sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(a)
 
+
+#Strings are Arrays
+
+a = "Hello, World!"
+print(a[1])
+
+
+#Looping Through a String
+for x in "python":
+  print(x)
+  
+  
+#String Length
+a = "Hello, World!"
+print(len(a))
+
+
+#Check String
+#Check if "free" is present in the following text:
+txt = "The best things in life are free!"
+print("the" in txt)
+
+
+#Use it in an if statement:
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
